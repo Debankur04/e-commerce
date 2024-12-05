b0a2a8332b0e734fae3dc4e0f989611ba8a87a15
Debankur04 <134576718+Debankur04@users.noreply.github.com>
2024-12-05 21:09:57 +0530

    Add files via upload

--- src/app/Components/Profile.jsx
+++ src/app/Components/Profile.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import Image from 'next/image' 
 const Profile = () => {
   return (
-    <div></div>
+    <div className='flex justify-around'>
+      <Image
+        src="/ChrisCrossPlainNavyBlueT-Shirt.jpg"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+        className='my-2 h-1/2 rounded'
+      />
+      <div>
+        <div className='font-bold text-3xl'>Chris Cross Plain Navy Blue T-Shirt</div>
+        <div className='font-semibold text-2xl'>250</div>
+        <div className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere quasi iste vitae! Tenetur quo impedit repudiandae nesciunt placeat atque.</div>
+        <div className='font-semibold'>Select Size</div>
+        <div className='flex gap-12'>
+          <div className='bg-gray-500 flex justify-center items-center h-8 w-8'>S</div>
+          <div className='bg-gray-500 flex justify-center items-center h-8 w-8'>L</div>
+          <div className='bg-gray-500 flex justify-center items-center h-8 w-8'>XL</div>
+        </div>
+        <button className='p-1 w-36 bg-black text-white'>Add To Cart</button>
+        <hr />
+        <div className='text-gray-500'>100% Cotton Product.</div>
+        <div className='text-gray-500'>Cash on Delivery.</div>
+        <div className='text-gray-500'>Easy Return Policy.</div>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Product from "./Components/Product";
+import Profile from "./Components/Profile";
 import Signin from "./Components/Signin";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <Product/>
+      <Profile/>
       <Footer/>
     </div>
   );
